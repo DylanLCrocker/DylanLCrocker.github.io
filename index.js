8aa90b36f2a48da1503426efc70bef2bd621a805
DylanLCrocker <dylan.crocker28@student.tamdistrict.org>
2025-12-15 00:43:40 +0000

    bug fix

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-let fontSize = 10;
-const backBtn = document.getElementById("backBtn");
-const homeBtn = document.getElementById("homeBtn");
 
-backBtn.addEventListener("click", () => {
-history.back(-1);
-});
+/* 
 
-homeBtn.addEventListener("click", () => {
-"../index.html";
-});
+ to add ballon game back
 
-
+ let fontSize = 10;
 
 let balloonEl = document.getElementById("balloon")
-function handleKeyDown(event) {
-  if (event.key == "ArrowUp") {
+function ballonKeyDown(event) {
+  if (event.key == "o") {
     fontSize *= 1.1;
     balloonEl.style.fontSize = fontSize + "px";
     event.preventDefault();
 
   }
-  if (event.key == "ArrowDown") {
+  if (event.key == "p") {
     fontSize /= 1.1;
     balloonEl.style.fontSize = fontSize + "px";
     event.preventDefault();
   }
 }
-window.addEventListener("keydown", handleKeyDown);
+window.addEventListener("keydown", ballonKeyDown);
+
+Paste in HTML:
+
+    <p id="balloon">🎈</p>
+    <script src="index.js"></script>
+    
+    */
