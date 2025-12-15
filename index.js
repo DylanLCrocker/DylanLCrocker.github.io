34b6310370d85f195cd10f8a3c0777aad1aee960
DylanLCrocker <dylan.crocker28@student.tamdistrict.org>
2025-12-15 00:15:47 +0000

    add back and home btns

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
- let fontSize = 10;
-  let balloonEl = document.getElementById("balloon")
-  function handleKeyDown(event) {
-    if (event.key == "ArrowUp") { 
-      fontSize *= 1.1;
-      balloonEl.style.fontSize = fontSize +"px";
-      event.preventDefault();
-      
-    }
-    if (event.key == "ArrowDown") {
-      fontSize /= 1.1;
-      balloonEl.style.fontSize = fontSize + "px";
-      event.preventDefault();
-        }
-    }
-  window.addEventListener("keydown", handleKeyDown);
+let fontSize = 10;
+const backBtn = document.getElementById("backBtn");
+const homeBtn = document.getElementById("homeBtn");
+
+backBtn.addEventListener("click", () => {
+javascript:history.back(-1);
+});
+
+homeBtn.addEventListener("click", () => {
+"../index.html";
+});
+
+
+
+let balloonEl = document.getElementById("balloon")
+function handleKeyDown(event) {
+  if (event.key == "ArrowUp") {
+    fontSize *= 1.1;
+    balloonEl.style.fontSize = fontSize + "px";
+    event.preventDefault();
+
+  }
+  if (event.key == "ArrowDown") {
+    fontSize /= 1.1;
+    balloonEl.style.fontSize = fontSize + "px";
+    event.preventDefault();
+  }
+}
+window.addEventListener("keydown", handleKeyDown);
